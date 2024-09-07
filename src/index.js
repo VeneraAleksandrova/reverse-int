08685a720ab86a634a3e0d010ae264b8729a5e1e
Faizieva Venera <v.faizyeva@gmail.com>
2024-09-07 13:39:56 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return +Array.from(String(Math.abs(n)))
+        .map((n, i, arr) => arr[arr.length - i - 1])
+        .join("");
+};

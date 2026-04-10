c5f5da0a77732921e41df29f493f77e2d15f8edd
Venera Aleksandrova <venera.n.aleksandrova@gmail.com>
2026-04-10 14:12:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  return Array.from(String(Math.abs(n)))
+    .map((el, i, arr) => arr[arr.length - i - 1])
+    .join('');
 };
